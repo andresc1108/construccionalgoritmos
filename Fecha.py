079fb595f7eb91c83648026f0fd31f275fbbd69d
andresc1108 <160885149+andresc1108@users.noreply.github.com>
2024-02-28 12:55:55 -0500

    Fecha.py

---
+++ Fecha.py
@@ -0,0 +1,17 @@
+class Fecha:
+    """----------------------------------
+    -------------------------------------"""
+    dia = 0
+    mes = 0
+    anio = 0
+    def ConsultarDia(self):
+    # Aqui va el codigo
+        return self.dia
+
+    def ConsultarMes(self):
+    # Aqui va el codigo
+        return self.mes
+
+    def ConsultarAnio(self):
+    # Aqui va el codigo
+        return self.anio
