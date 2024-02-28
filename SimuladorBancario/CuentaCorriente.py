175dc161415d76993d5fc1120c511b871091d96f
andresc1108 <160885149+andresc1108@users.noreply.github.com>
2024-02-28 12:54:42 -0500

    Update CuentaCorriente.py

--- SimuladorBancario/CuentaCorriente.py
+++ SimuladorBancario/CuentaCorriente.py
@@ -1,4 +1,22 @@
 class CuentaCorriente:
-    """
-    Atributos"""
-saldo=0
+    """----------------------------------
+    #Atributos
+    -------------------------------------"""
+    saldo= 0
+
+    """----------------------------------
+    Metodos
+    -------------------------------------"""
+    def ConsultarSaldo(self):
+        #Aquiva el codigo del metodo
+        return self.saldo
+    
+    def ConsignarValor(self,saldo):
+        nSaldo = self.saldo + ""
+        self.saldo = nSaldo
+        return "ingrese un valor" + ""
+    
+    def RetirarValor(self,saldo):
+        nSaldo = self.saldo + ""
+        self.saldo = nSaldo
+        return "ingrese valor a retirar" + ""
