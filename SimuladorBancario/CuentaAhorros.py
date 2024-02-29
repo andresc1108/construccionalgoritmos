d1db1dde71f3fe4e6ccf60e3ae00603fc926aa7f
andresc1108 <160885149+andresc1108@users.noreply.github.com>
2024-02-29 11:09:09 -0500

    Update CuentaAhorros.py

--- SimuladorBancario/CuentaAhorros.py
+++ SimuladorBancario/CuentaAhorros.py
@@ -1,10 +1,29 @@
-class CuentaAhorro:
-    """--------------------------
-    #Atributos
-    -----------------------------"""
-    Saldo= 0
-    InteresMensual= 0
+class CuentaAhorros:
     
-     def ConsultarSaldo(self):
-        #Aquiva el codigo del metodo
+    saldo = 0
+    interesMensual = 0
+    
+    '''----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------'''
+    
+    def ConsultarSaldo(self):
         return self.saldo
+    
+    def ConsignarMonto(self, monto):
+        # #Forma 1
+        # self.saldo += monto
+        # # Forma 2
+        # self.saldo = self.saldo + monto
+        # # Forma 3
+        total = self.saldo + monto
+        self.saldo = total
+    
+    def RetirarMonto(self, monto):
+        # #Forma 1
+        # self.saldo -= monto
+        # # Forma 2
+        # self.saldo = self.saldo - monto
+        # # Forma 3
+        total = self.saldo - monto
+        self.saldo = total
