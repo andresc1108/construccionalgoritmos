a36fd0a27e1ee3a06628d6fb5005fcef53097940
andresc1108 <160885149+andresc1108@users.noreply.github.com>
2024-03-21 12:29:16 -0500

    Create Tienda.py

---
+++ Tienda.py
@@ -0,0 +1,19 @@
+from producto import producto
+
+class Tienda:
+
+    """----------------------------------
+    #Atributos
+    ------------------------------------"""
+
+    __producto1 = None
+    __producto2 = None
+    __producto3 = None
+    __producto4 = None
+
+    """-------------------------------------------
+    #Metodos
+    ----------------------------------------------"""
+    
+    def getProducto1(self):
+        
