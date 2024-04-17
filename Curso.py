0091c7ab0ad8db92d7d3f2e2d1317e7fb2af5a6e
andresc1108 <160885149+andresc1108@users.noreply.github.com>
2024-04-17 12:22:52 -0500

    Create Curso.py

---
+++ Curso.py
@@ -0,0 +1,30 @@
+class Curso:
+
+    """____________________________________
+    #Atributos
+    _______________________________________"""
+
+
+    notaCurso = [4,5,3,2,3.5,1,3.5,4,3.5,5,4.5,4.2]
+
+
+
+
+    def numeroDeArreglos(self):
+        numeroArreglos = len (self.notaCurso)
+        return numeroArreglos 
+
+
+    def promedio2(self):
+
+
+
+
+        def proemedio3(self):
+
+            suma=0.0
+            indice=0
+
+            while indice<=12:
+                suma+=self.__nota[indice]
+                indice+=1
