e538251e8f5b6031af0b540108a841910524ecbb
andresc1108 <160885149+andresc1108@users.noreply.github.com>
2024-04-29 12:34:44 -0500

    Create Pasajero.py

---
+++ Pasajero.py
@@ -0,0 +1,10 @@
+class pasajero:
+
+    def __init__(self,pNombre,pCedula):
+        self.__cedula=self.pCedula
+        self.__nombre=self.pNombre
+    def darCedula(self):
+        return self.__cedula
+    def darNombre(self):
+        return self.__nombre
+    
